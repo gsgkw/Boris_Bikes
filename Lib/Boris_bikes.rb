47f6cd87817a1cfbcd5712f655679b2ad438309e
Guy Kemball-Williams <gsgkw@hotmail.co.uk>
2017-08-02 15:43:25 +0100

    17 complete

--- Lib/Boris_bikes.rb
+++ Lib/Boris_bikes.rb
@@ -2,11 +2,38 @@
 
 class DockingStation
 
-def release_bike
-  Bike.new
+attr_reader :bikes, :bike, :capacity
+DEFAULT_CAPACITY = 20
+
+def initialize(capacity = DEFAULT_CAPACITY)
+  @bikes = []
+  @capacity = capacity
 end
 
+def release_bike
+  raise 'there are no bikes' if empty?
+@bikes.pop
 end
+
+def dock_bike(bike)
+raise 'this dock is full' if full?
+@bikes << bike
+end
+
+private
+def full?
+  @bikes.size == DEFAULT_CAPACITY
+end
+
+def empty?
+  @bikes.size == 0
+end
+
+
+
+
+end
+
 
 class Bike
 
--- spec/Docking_Station_spec.rb
+++ spec/Docking_Station_spec.rb
@@ -1,7 +1,31 @@
 require 'Boris_bikes'
+
 describe DockingStation do
-it 'responds to release_bike' do
-#docking_station = DockingStation.new
-  expect(DockingStation.new.release_bike.class).to eq Bike
+"it 'responds to release_bike' do
+docking_station = DockingStation.new
+  expect(DockingStation.new.release_bike.class).to eq [Bike]
 end
+"
+it 'raises error when release bike called and no bike' do
+  docking_station = DockingStation.new
+  expect{docking_station.release_bike}.to raise_error("there are no bikes")
+end
+
+it 'responds to dock_bike' do
+  docking_station = DockingStation.new
+  DockingStation::DEFAULT_CAPACITY.times { docking_station.dock_bike(Bike.new) }
+  expect{docking_station.dock_bike(Bike.new)}.to raise_error("this dock is full")
+end
+
+it 'docking_station has non-default capacity' do
+  docking_station = DockingStation.new(30)
+  expect(docking_station.capacity).to eq 30
+end
+
+it 'docking_station has default capacity' do
+  docking_station = DockingStation.new
+  expect(docking_station.capacity).to eq 20
+end
+
+
 end
